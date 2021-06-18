2dfeab8054d5a6df9a517d65e60c19da341a8bdf
Thuan Saraiva <thuan.saraiva@pathfind.com.br>
2021-06-18 15:53:12 -0300

    rabbithealthindicator check

--- src/main/java/com/auditcom/validationconsumer/config/MessageRabbitMQConfig.java
+++ src/main/java/com/auditcom/validationconsumer/config/MessageRabbitMQConfig.java
@@ -1,14 +1,12 @@
 package com.auditcom.validationconsumer.config;
 
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,12 +34,17 @@ public class MessageRabbitMQConfig {
     }
 
     @Bean
+    public HealthIndicator rabbitHealthIndicator() {
+        return () -> Health.up().withDetail("version", "mock").build();
+    }
+
+    @Bean
     public MessageConverter converter() {
         return new Jackson2JsonMessageConverter();
     }
 
     @Bean
-    public AmqpTemplate temnplate(ConnectionFactory connectionFactory) {
+    public AmqpTemplate template(ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(converter());
         return rabbitTemplate;
